3de9082db52eb80266934dac74dba62fa6cb0563
andrewni <ajni@ucdavis.edu>
2018-05-04 03:45:57 -0700

    global config class finished

--- pixis/config.py
+++ pixis/config.py
@@ -2,6 +2,8 @@ import importlib.util
 import os
 import sys
 
+import yaml
+
 
 class Config:
     APPLICATION = 'flask'
@@ -20,6 +22,55 @@ class Config:
     # unsure what we're going to do with these
     FLASK_SERVER_NAME = 'flask_server'
     FLASK_SERVER_OUTPUT = PATH_OUT + os.path.sep + FLASK_SERVER_NAME
+    # WILL BE MOVED DURING LANGUAGE PART
+    JAVASCRIPT_TYPE_MAPPING = {
+        'integer': 'number',
+        'int32': 'number',
+        'long': 'number',
+        'int64': 'number',
+        'float': 'number',
+        'double': 'number',
+        'string': 'string',
+        'byte': 'string',
+        'binary': 'string',
+        'boolean': 'boolean',
+        'date': 'string',
+        'date-time': 'Date',
+        'password': 'string',
+        'object': 'any',  # TODO
+        'array': 'Array',
+        '<': '<',
+        '>': '>',
+    }
+
+    PYTHON_TYPE_MAPPING = {
+        'integer': 'int',
+        'int32': 'int',
+        'long': 'int',
+        'int64': 'int',
+        'float': 'float',
+        'double': 'float',
+        'string': 'str',
+        # 'byte': 'ByteArray',
+        'byte': 'str',
+        'binary': 'str',
+        # 'binary': 'Binary',
+        'boolean': 'bool',
+        'date': 'date',
+        'date-time': 'datetime',
+        'password': 'str',
+        'object': 'object',  # TODO
+        'array': 'List',
+        '<': '[',
+        '>': ']',
+    }
+
+    TYPE_MAPPINGS = {
+        'python': PYTHON_TYPE_MAPPING,
+        'flask': PYTHON_TYPE_MAPPING,
+        'javascript': JAVASCRIPT_TYPE_MAPPING,
+        'typescript': JAVASCRIPT_TYPE_MAPPING,
+    }
 
     @staticmethod
     def load_build_file(build_file):  # build_file should be a relative filepath
@@ -40,6 +91,9 @@ class Config:
         Config.PATH_TEMPLATES = os.getcwd() + os.path.sep + Config.TEMPLATES
         Config.PATH_OUT = os.getcwd() + os.path.sep + Config.OUT
 
+        Config.FLASK_SERVER_NAME = 'flask_server'
+        Config.FLASK_SERVER_OUTPUT = Config.PATH_OUT + os.path.sep + Config.FLASK_SERVER_NAME
+
     @staticmethod
     def load_spec_file():
         with open(Config.PATH_SPEC) as f:
@@ -50,60 +104,9 @@ class Config:
                     Config.SPEC_DICT = json.load(f)
                 except ValueError as json_error:
                     extension = os.path.splitext(Config.PATH_SPEC)[1][1:]
-                    if extension = 'json':
+                    if extension == 'json':
                         print(json_error)
                         sys.exit()
                     else:
                         print(yaml_error)
                         sys.exit()
-
-
-
-JAVASCRIPT_TYPE_MAPPING = {
-    'integer': 'number',
-    'int32': 'number',
-    'long': 'number',
-    'int64': 'number',
-    'float': 'number',
-    'double': 'number',
-    'string': 'string',
-    'byte': 'string',
-    'binary': 'string',
-    'boolean': 'boolean',
-    'date': 'string',
-    'date-time': 'Date',
-    'password': 'string',
-    'object': 'any',  # TODO
-    'array': 'Array',
-    '<': '<',
-    '>': '>',
-}
-
-PYTHON_TYPE_MAPPING = {
-    'integer': 'int',
-    'int32': 'int',
-    'long': 'int',
-    'int64': 'int',
-    'float': 'float',
-    'double': 'float',
-    'string': 'str',
-    # 'byte': 'ByteArray',
-    'byte': 'str',
-    'binary': 'str',
-    # 'binary': 'Binary',
-    'boolean': 'bool',
-    'date': 'date',
-    'date-time': 'datetime',
-    'password': 'str',
-    'object': 'object',  # TODO
-    'array': 'List',
-    '<': '[',
-    '>': ']',
-}
-
-TYPE_MAPPINGS = {
-    'python': PYTHON_TYPE_MAPPING,
-    'flask': PYTHON_TYPE_MAPPING,
-    'javascript': JAVASCRIPT_TYPE_MAPPING,
-    'typescript': JAVASCRIPT_TYPE_MAPPING,
-}
