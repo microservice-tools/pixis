01cf5d455d5a74a03444cc598b4755083dd2d1d5
andrewni <ajni@ucdavis.edu>
2018-05-06 03:24:40 -0700

    Implementation base class

---
+++ pixis/implementations/implementation.py
@@ -0,0 +1,23 @@
+class Implementation():
+    @staticmethod
+    def generate_custom():
+        pass
+
+    @staticmethod
+    def generate_once():
+        pass
+
+    @staticmethod
+    def generate_per_schema():
+        pass
+
+    @staticmethod
+    def generate_per_tag():
+        pass
+
+    @staticmethod
+    def lower_first(s):
+        return s[:1].lower() + s[1:] if s else ''
+
+    def stage_default_iterators():
+        pass
