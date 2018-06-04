42848a2e7c63471291fc3be6c53b7afae8e5c1e5
andrewni <ajni@ucdavis.edu>
2018-06-04 04:40:16 -0700

    added OVERWRITE option to command line

--- pixis/main.py
+++ pixis/main.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument('-o', '--output', default='build', help="Set output directory location, default: %(default)s", dest='output')
     parser.add_argument('-t', '--templates', default='templates', help="Set local template directory, default: %(default)s", dest='templates')
     parser.add_argument('-v', '--verbose', default=False, help="Set verbose mode, default: %(default)s", dest='verbose')
+    parser.add_argument('-w', '--overwrite', default=False, help="Set force overwrite mode, default: %(default)s", dest='overwrite')
 
     # add back later if we're going to be implementing quiet
     # group = parser.add_mutually_exclusive_group(required=False)
@@ -17,10 +18,11 @@ def main():
     # group.add_argument('-v', '--verbose', help="Increase output verbosity", action='store_true', dest='verbose')
     args = parser.parse_args()
 
-    utils.set_config('build', args.build_file)
-    utils.set_config('templates', args.templates)
-    utils.set_config('out', args.output)
-    utils.set_config('verbose', args.verbose)
+    utils.set_config('BUILD', args.build_file)
+    utils.set_config('TEMPLATES', args.templates)
+    utils.set_config('OUTPUT', args.output)
+    utils.set_config('VERBOSE', args.verbose)
+    utils.set_config('OVERWRITE', args.overwrite)
 
     try:
         utils.load_build_file(args.build_file)
