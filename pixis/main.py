616fcc15b006c6a72053f1834233bb56579f4ac8
andrewni <ajni@ucdavis.edu>
2018-07-31 18:54:04 -0700

    styling

--- pixis/main.py
+++ pixis/main.py
@@ -6,11 +6,26 @@ import pixis.utils as utils
 
 def main():
     parser = argparse.ArgumentParser(description='A rest api code generator')
-    parser.add_argument('-b', '--build', default='build.py', help="Set build file location, default: %(default)s", dest='build_file')
-    parser.add_argument('-o', '--output', default='build', help="Set output directory location, default: %(default)s", dest='output')
-    parser.add_argument('-t', '--templates', default='templates', help="Set local template directory, default: %(default)s", dest='templates')
-    parser.add_argument('-v', '--verbose', default=False, help="Set verbose mode, default: %(default)s", dest='verbose')
-    parser.add_argument('-w', '--overwrite', default=False, help="Set force overwrite mode, default: %(default)s", dest='overwrite')
+    parser.add_argument('-b', '--build',
+                        default='build.py',
+                        help="Set build file location, default: %(default)s",
+                        dest='build_file')
+    parser.add_argument('-o', '--output',
+                        default='build',
+                        help="Set output directory location, default: %(default)s",
+                        dest='output')
+    parser.add_argument('-t', '--templates',
+                        default='templates',
+                        help="Set local template directory, default: %(default)s",
+                        dest='templates')
+    parser.add_argument('-v', '--verbose',
+                        default=False,
+                        help="Set verbose mode, default: %(default)s",
+                        dest='verbose')
+    parser.add_argument('-w', '--overwrite',
+                        default=False,
+                        help="Set force overwrite mode, default: %(default)s",
+                        dest='overwrite')
 
     # add back later if we're going to be implementing quiet
     # group = parser.add_mutually_exclusive_group(required=False)
